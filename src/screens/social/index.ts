589a7c302c2ec054ffdd46c765bb07d5c1239c6b
Dương Ngọc Quang Khiêm <128200532+KhimDuong@users.noreply.github.com>
2026-04-28 23:58:12 +0700

    FriendTrackingScreen

--- src/screens/social/index.ts
+++ src/screens/social/index.ts
@@ -1,2 +1,3 @@
 export { default as SocialChatScreen } from './ChatScreen';
+export { default as FriendTrackingScreen } from './FriendTrackingScreen';
 export { default as MessageListScreen } from './MessageListScreen';
