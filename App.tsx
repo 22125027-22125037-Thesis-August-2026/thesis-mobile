510ce996a50b9da946f038deda7ea95f6c310359
Dương Ngọc Quang Khiêm <128200532+KhimDuong@users.noreply.github.com>
2026-04-28 23:55:51 +0700

    FriendTrackingScreen}

--- App.tsx
+++ App.tsx
@@ -20,6 +20,7 @@ import {
   DiaryEntryScreen,
   DiaryOverviewScreen,
   FoodMainScreen,
+  FriendTrackingScreen,
   HomeScreen,
   LoginScreen,
   MatchingFormScreen,
@@ -137,6 +138,7 @@ const renderTeenExperienceRoutes = () => {
       <Stack.Screen name="DiaryEntry" component={DiaryEntryScreen} />
       <Stack.Screen name="SleepMain" component={SleepMainScreen} />
       <Stack.Screen name="FoodMain" component={FoodMainScreen} />
+      <Stack.Screen name="FriendTracking" component={FriendTrackingScreen} />
     </>
   );
 };
